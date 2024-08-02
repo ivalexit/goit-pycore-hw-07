a736cb14beb0d1fe3664a39e46eb847dd2ba0586
Oleksii Ivanenko <ivalextar@gmail.com>
2024-08-02 09:57:56 +0200

    class Field

--- bot_birthday.py
+++ bot_birthday.py
@@ -3,3 +3,15 @@ import sys
 import os
 from collections import UserDict
 from datetime import datetime, timedelta
+
+
+# Базовий клас для полів контакту
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+    
+# Клас для збереження імені контакту
+class Name(Field):
