1d351abb6ad23506ab7c4cc6e6aaaef6bf3be793
Oleksii Ivanenko <ivalextar@gmail.com>
2024-08-02 10:51:46 +0200

    searching number

--- bot_birthday.py
+++ bot_birthday.py
@@ -60,3 +60,18 @@ class Record:
                 self.phones.remove(p)
                 return True
         return False
+    
+    # Редагування номера телефону
+    def edit_phone(self, old_phone, new_phone):
+        for i, p in enumerate(self.phones):
+            if p.value == old_phone:
+                self.phones[i] = Phone(new_phone)
+                return True
+        return False
+    
+    # Пошук номера телефону
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
