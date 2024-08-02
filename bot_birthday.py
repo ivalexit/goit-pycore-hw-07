f0e7254ae04d0c2023f920659665a47af4fcc759
Oleksii Ivanenko <ivalextar@gmail.com>
2024-08-02 10:12:22 +0200

    class Phone and validation

--- bot_birthday.py
+++ bot_birthday.py
@@ -19,3 +19,14 @@ class Name(Field):
         if not value:
             raise ValueError('Name cannot be empty')
         super().__init__(value)
+
+# Клас для збереження та валідації номера телефону
+class Phone(Field):
+    def __init__(self, value):
+        if not self.validate_phone(value):
+            raise ValueError('Phone number must have 10 digits')
+        super().__init__(value)
+
+    @staticmethod
+    def validate_phone(value):
+        return value.isdigit() and len(value) == 10
